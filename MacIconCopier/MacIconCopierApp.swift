2ba9a89db43cc680f18fbbc588deb8fcd5d3bdb3
Rose Kay <HeyRoseKay@icloud.com>
2025-07-11 23:02:20 -0700

    Initial Commit

---
+++ MacIconCopier/MacIconCopierApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacIconCopierApp.swift
+//  MacIconCopier
+//
+//  Created by Rose Kay on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MacIconCopierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
